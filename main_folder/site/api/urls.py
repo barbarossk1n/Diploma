d6b04b0d75e2c3acc244d5e50dfda7b361a72a75
vingardium-leviosa <145845083+vingardium-leviosa@users.noreply.github.com>
2025-05-19 13:02:44 +0300

    Delete main_folder/site/api directory

--- main_folder/site/api/urls.py
+++
@@ -1,9 +0,0 @@
-# api/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # API endpoints будут добавлены позже
-    path('user-predictions/', views.user_predictions, name='user_predictions'),
-    path('prediction-detail/<int:prediction_id>/', views.prediction_detail, name='prediction_detail'),
-]
